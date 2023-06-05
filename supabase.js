25ed7d799655d6e974923504526b4c3670410ee4
Nikolett Dékány <niko703j@stud.kea.dk>
2023-06-05 22:55:16 +0200

    new tables connection

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://xntlptjhmiswskncddwy.supabase.co";
+const SUPABASE_URL = "https://rgcrqxuogzyelzahrnxq.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhudGxwdGpobWlzd3NrbmNkZHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MTA2MTUsImV4cCI6MTk5OTk4NjYxNX0.WnxT09BuWLVdEcdSHrve0uib4_Zzu7l1VUY99PI8UaE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnY3JxeHVvZ3p5ZWx6YWhybnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5OTc2NDUsImV4cCI6MjAwMTU3MzY0NX0.K-puymaqNm7AFvZno88eGz0yV8ce5pzSiB_EcCfznmc";
 
 // Initialize the Supabase client
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
