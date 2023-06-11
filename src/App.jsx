6e91d7d56bc0ee8dd1151e1024890076a81e81bd
Nikolett Dékány <niko703j@stud.kea.dk>
2023-06-11 20:47:24 +0200

    login fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,22 @@ import FinishedPage from "./pages/FinishedPage";
 import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 const App = ({ handleLogin }) => {
-  // const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      {/* {user ? ( */}
-      <>
-        <Route path="/profile" element={<UserProfilePage />} />
-        <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/lesson/:courseId/:lessonId" element={<LessonPage />} />
-        <Route path="/finished/:courseId" element={<FinishedPage />} />
-      </>
-      {/* ) : (
+      {user ? (
+        <>
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/lesson/:courseId/:lessonId" element={<LessonPage />} />
+          <Route path="/finished/:courseId" element={<FinishedPage />} />
+        </>
+      ) : (
         <Route path="/" element={<Navigate to="/login" replace />} />
-      )} */}
+      )}
     </Routes>
   );
 };
